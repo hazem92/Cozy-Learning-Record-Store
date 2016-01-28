81e20e6acb594e72fbdb024f1405343fc2b54107
zelong <zelong.chen@applidget.com>
2016-01-28 18:42:07 +0100

    update statement controllers

--- server/controllers/statement.js
+++ server/controllers/statement.js
@@ -109,10 +109,192 @@ router.delete('/statements/:id', function(req, res, next) {
 });
 
 
+/// List of all statements
+router.get('/statements', function(req, res, next) {
+
+    Statement.request('all', function(err, statements) {
+        if(err) {
+            /*
+                If an unexpected error occurs, forward it to Express error
+                middleware which will send the error properly formatted.
+            */
+            next(err);
+        } else {
+            /*
+                If everything went well, send an empty response with the correct
+                HTTP status.
+            */
+            res.status(200).json(statements);
+        }
+    });
+});
+
+/// List of all statements, for a given actor name
+router.get('/statements/:actor_name', function(req, res, next) {
+
+// could be changed to fit the view in request0j parameters
+    var options =  {
+        key: req.params.actor_name
+    };
+    Actor.request('byName', options, function(err, actor){
+      if(err) {
+        /*
+            If an unexpected error occurs, forward it to Express error
+            middleware which will send the error properly formatted.
+        */
+        next(err);
+      } else if(!actor) {
+        /*
+            If there was no unexpected error, but that the document has not
+            been found, send the legitimate status code. `statement` is null.
+        */
+        res.sendStatus(404);
+      } else {
+        /*
+            If everything went well, send an empty response with the correct
+            HTTP status.
+        */
+        var option =  {
+            key: actor
+        };
+
+        Statement.request('byActor', option, function(err, statements) {
+            if(err) {
+                /*
+                    If an unexpected error occurs, forward it to Express error
+                    middleware which will send the error properly formatted.
+                */
+                next(err);
+            } else if(!statements) {
+                /*
+                    If there was no unexpected error, but that the document has not
+                    been found, send the legitimate status code. `statement` is null.
+                */
+                res.sendStatus(404);
+            }  else {
+                /*
+                    If everything went well, send an empty response with the correct
+                    HTTP status.
+                */
+                res.status(200).json(statements);
+            }
+        });
+      }
+    });
+});
+
+/// List of all statements, for a given activity name
+router.get('/statements/:activity_name', function(req, res, next) {
+
+// could be changed to fit the view in request0j parameters
+    var options =  {
+        key: req.params.activity_name
+    };
+    Activity.request('byName', options, function(err, activity){
+      if(err) {
+        /*
+            If an unexpected error occurs, forward it to Express error
+            middleware which will send the error properly formatted.
+        */
+        next(err);
+      } else if(!activity) {
+        /*
+            If there was no unexpected error, but that the document has not
+            been found, send the legitimate status code. `statement` is null.
+        */
+        res.sendStatus(404);
+      } else {
+        /*
+            If everything went well, send an empty response with the correct
+            HTTP status.
+        */
+        var option =  {
+            key: activity
+        };
+
+        Statement.request('byActivity', option, function(err, statements) {
+            if(err) {
+                /*
+                    If an unexpected error occurs, forward it to Express error
+                    middleware which will send the error properly formatted.
+                */
+                next(err);
+            } else if(!statements) {
+                /*
+                    If there was no unexpected error, but that the document has not
+                    been found, send the legitimate status code. `statement` is null.
+                */
+                res.sendStatus(404);
+            }  else {
+                /*
+                    If everything went well, send an empty response with the correct
+                    HTTP status.
+                */
+                res.status(200).json(statements);
+            }
+        });
+      }
+    });
+});
+
+/// List of all statements, for a given verb
+router.get('/statements/:verb', function(req, res, next) {
+
+// could be changed to fit the view in request0j parameters
+    var options =  {
+        key: req.params.verb
+    };
+    Verb.request('byDisplay', options, function(err, verb){
+      if(err) {
+        /*
+            If an unexpected error occurs, forward it to Express error
+            middleware which will send the error properly formatted.
+        */
+        next(err);
+      } else if(!verb) {
+        /*
+            If there was no unexpected error, but that the document has not
+            been found, send the legitimate status code. `statement` is null.
+        */
+        res.sendStatus(404);
+      } else {
+        /*
+            If everything went well, send an empty response with the correct
+            HTTP status.
+        */
+        var option =  {
+            key: verb
+        };
+
+        Statement.request('byVerb', option, function(err, statements) {
+            if(err) {
+                /*
+                    If an unexpected error occurs, forward it to Express error
+                    middleware which will send the error properly formatted.
+                */
+                next(err);
+            } else if(!statements) {
+                /*
+                    If there was no unexpected error, but that the document has not
+                    been found, send the legitimate status code. `statement` is null.
+                */
+                res.sendStatus(404);
+            }  else {
+                /*
+                    If everything went well, send an empty response with the correct
+                    HTTP status.
+                */
+                res.status(200).json(statements);
+            }
+        });
+      }
+    });
+});
+/*
 /// List of all statements, for a given attribute !
 router.get('/statements', function(req, res, next) {
 
-// could be changed to fit the view in request0j parameters 
+// could be changed to fit the view in request0j parameters
     var options =  {
         key: 'Joseph'
     };
@@ -132,7 +314,6 @@ router.get('/statements', function(req, res, next) {
             res.status(200).json(statements);
         }
     });
-});
-
+});*/
 // Export the router instance to make it available from other files.
 module.exports = router;
