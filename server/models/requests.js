919078936e94dc56596ddc01e9ceb5339d96a951
zelong <zelong.chen@applidget.com>
2016-01-28 18:44:05 +0100

    update request

--- server/models/requests.js
+++ server/models/requests.js
@@ -5,26 +5,28 @@ var cozydb = require('cozydb');
 
 module.exports = {
 
- statement: {
-    all: cozydb.defaultRequests.all
-	  byActor: cozydb.defaultRequests.by('actor')
-    byActivity:cozydb.defaultRequests.by('activity')
+  statement: {
+    all: cozydb.defaultRequests.all,
+    byActor: cozydb.defaultRequests.by('actor'),
+    byActivity:cozydb.defaultRequests.by('activity'),
     byVerb:cozydb.defaultRequests.by('verb')
- }
+  }
 
- actor: {
-    all: cozydb.defaultRequests.all
-	  byMbox: cozydb.defaultRequests.by('mbox')
- }
+  actor: {
+    all: cozydb.defaultRequests.all,
+    byMbox: cozydb.defaultRequests.by('mbox'),
+    byName: cozydb.defaultRequests.by('name')
+  }
 
- verb: {
-    all: cozydb.defaultRequests.all
-    byId: cozydb.defaultRequests.by('id')
-}
+  verb: {
+    all: cozydb.defaultRequests.all,
+    byDisplay: cozydb.defaultRequests.by('display')
+  }
 
- activity: {
-    all: cozydb.defaultRequests.all
-    byId: cozydb.defaultRequests.by('id')
- }
+  activity: {
+    all: cozydb.defaultRequests.all,
+    byType: cozydb.defaultRequests.by('type'),
+    byName: cozydb.defaultRequests.by('name')
+  }
 
 };
