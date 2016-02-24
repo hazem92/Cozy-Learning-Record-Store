647764be293bba448e7a3d74a0f8b11e64e31d10
hamara <hazem.amara@telecom-bretagne.eu>
2016-02-24 16:54:40 +0100

    create views for public interface

--- server/controllers/public.js
+++ server/controllers/public.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var p_site = require('../public_site');
+var oauth2 = require ('../oauth2');
+var user = require ('../user');
+
 
 var verbController = require('./verb');
 router.use(verbController);
@@ -26,5 +30,21 @@ router.get('/public/verbs', function(req, res, next) {
   });
 
 });
+
+
+// Passport configuration
+require('../auth');
+
+router.get('/public', p_site.index);
+router.get('/public/login', p_site.loginForm);
+router.post('/public/login', p_site.login);
+router.get('/public/logout', p_site.logout);
+router.get('/public/account', p_site.account);
+
+router.get('/public/dialog/authorize', oauth2.authorization);
+router.post('/public/dialog/authorize/decision', oauth2.decision);
+router.post('/public/oauth/token', oauth2.token);
+
+router.get('/public/api/userinfo', user.info);
 
 module.exports = router;
